a81af8e3b0360ceb81c53a0a91f0fe29461f5623
birlikmuhammedtalha-gif <birlikmuhammedtalha@gmail.com>
2026-05-03 11:40:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const io = new Server(server, {
   cors: { origin: '*' }
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname)));
 
 // Oda yönetimi
 const rooms = {};
